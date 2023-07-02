0762b5572b02a17d13d1aa35154359a67b6e7803
Jim Carr <jim.carr@devtoprd.com>
2023-07-02 10:54:00 -0400

    Moved service settings to appsettings

--- src/BlazorClient/Pages/Sensor.razor.cs
+++ src/BlazorClient/Pages/Sensor.razor.cs
@@ -1,19 +1,50 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
 using SenseHatLib.Services;
 
 namespace BlazorClient.Pages
 {
+	public sealed class SensorServiceSettings
+	{
+		public required string UrlPrefix { get; set; }
+		public required string IpAddress { get; set; }
+		public required int Port { get; set; }
+	}
+
 	public partial class Sensor
 	{
+		[Inject]
+		private IConfiguration? configuration { get; set; }
+		private SensorServiceSettings sensorServiceSettings;
+
 		private string? currentTemperature;
 		private string? currentHumidity;
 		private string? currentAltitude;
 		private string? statusMessage;
 
+		private SensorServiceSettings GetSettings()
+		{
+			return new SensorServiceSettings()
+			{
+				UrlPrefix = configuration.GetValue<string>("SensorService:UrlPrefix"),
+				IpAddress = configuration.GetValue<string>("SensorService:IpAddress"),
+				Port = configuration.GetValue<int>("SensorService:Port")
+			};
+		}
+
+		private SenseHatClient GetSenseHatClient()
+		{
+			sensorServiceSettings = GetSettings();
+
+			return new SenseHatClient(sensorServiceSettings.UrlPrefix, sensorServiceSettings.IpAddress, sensorServiceSettings.Port);
+		}
+
 		private void GetSensorData()
 		{
 			try
 			{
-				var senseHatClient = new SenseHatClient("http", "192.168.0.140", 5191);
+				var senseHatClient = GetSenseHatClient();
+
 				var result = senseHatClient.GetSensorData(SenseHatLib.Helpers.MeasurementUnits.Imperial);
 
 				currentTemperature = result.Data.FormattedTemperature;
@@ -32,7 +63,7 @@ namespace BlazorClient.Pages
 		{
 			try
 			{
-				var senseHatClient = new SenseHatClient("http", "192.168.0.140", 5191);
+				var senseHatClient = GetSenseHatClient();
 
 				senseHatClient.SetLed(clearDisplay);
 
