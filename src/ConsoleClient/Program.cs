ea17f25ebb2ffb3611f5e1dfb65bc0bd9b55616d
Jim Carr <jim.carr@devtoprd.com>
2023-07-02 21:17:19 -0400

    New Console client.

---
+++ src/ConsoleClient/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Configuration;
+using SenseHatLib.Models;
+using SenseHatLib.Services;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		IConfiguration config = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json")
+			.AddEnvironmentVariables()
+			.Build();
+
+		var appSettings = config.GetRequiredSection("Settings").Get<AppSettings>();
+
+		if (args.Length == 1)
+		{
+			var myService = new SenseHatClient(appSettings.ServiceUrlPrefix, appSettings.ServiceIpAddress, appSettings.ServicePort);
+
+			SensorResult results;
+
+			switch (args[0].ToLower())
+			{
+				case "temperature":
+					results = myService.GetSensorData(SenseHatLib.Helpers.MeasurementUnits.Imperial);
+					Console.WriteLine((results.Status.IsValid) ? $"Temperature is {results.Data.FormattedTemperature}" : results.Status.ErrorMessage);
+					break;
+
+				case "humidity":
+					results = myService.GetSensorData(SenseHatLib.Helpers.MeasurementUnits.Imperial);
+					Console.WriteLine((results.Status.IsValid) ? $"Humidity is {results.Data.FormattedHumidity}" : results.Status.ErrorMessage);
+					break;
+
+				case "altitude":
+					results = myService.GetSensorData(SenseHatLib.Helpers.MeasurementUnits.Imperial);
+					Console.WriteLine((results.Status.IsValid) ? $"Altitude is {results.Data.FormattedAltitude}" : results.Status.ErrorMessage);
+					break;
+
+				case "led-white":
+					myService.SetLed(true);
+					break;
+
+				case "led-multi":
+					myService.SetLed(false);
+					break;
+
+				default:
+					ShowHelpMessage();
+					break;
+			}
+		}
+		else
+		{
+			ShowHelpMessage();
+		}
+	}
+
+	private static void ShowHelpMessage()
+	{
+		Console.WriteLine("Not sure what you want to do.  You can use any of these arguments: 'temperature', 'humidity', 'altitude', 'led-multi', or 'led-white'");
+	}
+}
+
+/// <summary>
+/// Holds settings retrieved from appsettings.json
+/// </summary>
+public sealed class AppSettings
+{
+	public required string ServiceUrlPrefix { get; set; }
+	public required string ServiceIpAddress { get; set; }
+	public required int ServicePort { get; set; }
+	public required int PollingIntervalInSeconds { get; set; }
+}
